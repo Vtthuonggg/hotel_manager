d6bd803689e0b7a170a6772d890e5b84dd1d0201
ThanhTung-2010 <dtong201003@gmail.com>
2024-09-09 18:10:41 +0700

    sua

--- src/components/navbar_page.vue
+++ src/components/navbar_page.vue
@@ -5,48 +5,14 @@
         <img src="../assets/logo.png" alt="Vue logo" class="logo" />
       </li>
       <li class="navbar-item">
-        <router-link to="/listroom"><i class="fas fa-door-open"></i><span class="bold-text"></span>PHÒNG</router-link>
+        <router-link to="/listroom"><i class="fas fa-door-open"></i><span class="bold-text"></span>Phòng</router-link>
       </li>
       <li class="navbar-item">
-        <router-link to="/reports"> <i class="fas fa-chart-bar"></i><span class="bold-text"></span>BÁO CÁO</router-link>
+        <router-link to="/reports"> <i class="fas fa-chart-bar"></i><span class="bold-text"></span>Báo cáo</router-link>
       </li>
-      <v-navigation-drawer
-      style="z-index: 1001"
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <template v-slot:prepend>
-        <v-list dense style="min-width: 250px !important">
-          <v-row class="mx-3 my-0">
-            <v-col cols="12" class="pb-1"
-            >
-              <div class="black--text fs-13">Chào ông / bà</div>
-            </v-col
-            >
-            <v-col cols="12" class="pt-0">
-              <div class="primary--text fs-16">
-                {{ username }}
-              </div>
-            </v-col>
-          </v-row>
-          <v-divider class="primary"></v-divider>
-          <v-list-item @click="toggleChangePass" class="d-flex align-center">
-            <v-list-item-icon class="mx-0">
-              <v-icon size="23">mdi-lock-reset</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title class="ml-1">Đổi mật khẩu</v-list-item-title>
-          </v-list-item>
-          <v-list-item @click="logout" class="d-flex align-center">
-            <v-list-item-icon class="mx-0">
-              <v-icon size="23">mdi-logout</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title class="ml-1">Đăng xuất</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </template>
-    </v-navigation-drawer>
+      <li class="navbar-item1">
+        <router-link to="/settings"><i class="fas fa-cog"></i><span class="bold-text"></span>Cài đặt</router-link>
+      </li>
     </ul>
   </nav>
 </template>
@@ -54,27 +20,6 @@
 <script>
 export default {
   name: "AppNavbar",
-  data() {
-    return {
-      showSettingsMenu: false,
-    };
-  },
-  methods: {
-    toggleSettingsMenu() {
-      this.showSettingsMenu = !this.showSettingsMenu;
-    },
-    handleClickOutside(event) {
-      if (!this.$el.contains(event.target)) {
-        this.showSettingsMenu = false;
-      }
-    },
-  },
-  mounted() {
-    document.addEventListener('click', this.handleClickOutside);
-  },
-  beforeDestroy() {
-    document.removeEventListener('click', this.handleClickOutside);
-  },
 };
 </script>
 
@@ -107,7 +52,7 @@ export default {
   border-radius: 5px;
   transition: box-shadow 0.3s ease;
   font-weight: bold;
-  font-size: 20px;
+  font-size: 25px;
 }
 .navbar-item1 a {
   color: #333;
@@ -117,7 +62,7 @@ export default {
   border-radius: 5px;
   transition: box-shadow 0.3s ease;
   font-weight: bold;
-  font-size: 20px;
+  font-size: 25px;
 }
 .navbar-item1 {
   margin-left: auto;
@@ -147,37 +92,5 @@ i {
   margin-right: 5px;
   font-size: 22px;
 }
-.settings-menu {
-  display: none;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  background-color: white;
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
 
-.settings-menu li {
-  margin: 10;
-}
-
-.settings-menu li a {
-  padding: 10px;
-  display: block;
-  text-decoration: none;
-  color: #333;
-  font-weight: normal;
-}
-
-.settings-menu li a:hover {
-  background-color: rgba(0, 0, 0, 0.1);
-}
-
-/* Show menu when `showSettingsMenu` is true */
-.navbar-item1 .settings-menu {
-  display: block;
-}
 </style>
