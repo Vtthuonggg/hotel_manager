62a77cc27f73535417572dc8a26f72830120a25c
Vtthuonggg <thuong23042003@gmail.com>
2024-09-11 11:09:01 +0700

    cai vuetify

--- src/components/room/list_room_page.vue
+++ src/components/room/list_room_page.vue
@@ -1,13 +1,50 @@
 <template>
     <div>
-        <i class="fas fa-star"></i>
-        <h2>ABCVBS</h2>
+
+
+        <h1>Danh sách phòng</h1>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="12" md="4" v-for="room in rooms" :key="room.id">
+          <v-card>
+          
+            <v-card-title>
+              <h3>{{ room.name }}</h3>
+            </v-card-title>
+            <v-card-text>
+              <p>{{ room.description }}</p>
+              <p>Giá: {{ room.price }}</p>
+            </v-card-text>
+            <v-card-actions>
+              <router-link :to="'/room/' + room.id">
+                Xem chi tiết
+              </router-link>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'ListRoomPage'
+    data(){
+        return{
+            rooms:[{
+id: 1,name: "P102", 
+description:"Phòng đơn",
+ "pice":1000,
+            },{
+id: 1,name: "P102", 
+description:"Phòng đơn",
+ "pice":1000,
+            },{
+id: 1,name: "P102", 
+description:"Phòng đơn",
+ "pice":1000,
+            }]
+        }
+    }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import vuetify from "./plugin/vuetify";
+import Vuetify from "vuetify";
 import router from "./router";
 import Toast from "vue-toastification";
-
+import "vuetify/dist/vuetify.min.css";
+import "vue-toastification/dist/index.css";
+Vue.use(Vuetify);
 Vue.config.productionTip = false;
 Vue.use(Toast, {
   duration: 2000,
@@ -12,6 +14,6 @@ Vue.use(Toast, {
 
 new Vue({
   router,
-  vuetify,
+  vuetify: new Vuetify(),
   render: (h) => h(App),
 }).$mount("#app");
