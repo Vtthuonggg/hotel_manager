6a758e3942de879abb7068a42960b77f97a58a0c
Vu Thanh Thuong <thuong23042003@gmail.com>
2024-09-12 00:57:10 +0700

    info setting

--- src/components/navbar_page.vue
+++ src/components/navbar_page.vue
@@ -34,10 +34,8 @@
           <span class="bold-text">Cài đặt</span>
         </a>
         <ul class="dropdown-menu" v-if="dropdownVisible">
-          <li>
-            <router-link to="/settings/info">
-              <i class="fa-solid fa-hotel"></i>Cài đặt thông tin</router-link
-            >
+          <li @click="showInputInfo">
+            <a href="#"><i class="fa-solid fa-hotel"></i>Cài đặt thông tin</a>
           </li>
           <li @click="showPriceModal">
             <a href="#"> <i class="fa-solid fa-coins"></i>Cài đặt giá phòng</a>
@@ -57,37 +55,85 @@
         <h2 class="title">Cài đặt giá phòng</h2>
         <div class="price-setting">
           <label for="single-room">Phòng đơn:</label>
-          <input type="text" id="single-room" v-model="singleRoomPrice" />
+          <input
+            type="text"
+            id="single-room"
+            v-model="roomPrice.singleRoomPrice"
+          />
         </div>
         <div class="price-setting">
           <label for="double-room">Phòng đôi:</label>
-          <input type="text" id="double-room" v-model="doubleRoomPrice" />
+          <input
+            type="text"
+            id="double-room"
+            v-model="roomPrice.doubleRoomPrice"
+          />
         </div>
         <div class="price-setting">
           <label for="triple-room">Phòng ba:</label>
-          <input type="text" id="triple-room" v-model="tripleRoomPrice" />
+          <input
+            type="text"
+            id="triple-room"
+            v-model="roomPrice.tripleRoomPrice"
+          />
         </div>
         <div class="price-setting">
           <label for="quad-room">Phòng bốn:</label>
-          <input type="text" id="quad-room" v-model="quadRoomPrice" />
+          <input type="text" id="quad-room" v-model="roomPrice.quadRoomPrice" />
         </div>
         <button class="save-button" @click="savePrices">Lưu</button>
       </div>
+    </div>
+    <div v-if="isInputInfo" class="form-container">
+      <form>
+        <v-row>
+          <div class="logo">
+            <img alt="Vue logo" src="../assets/logo.png" class="logo" />
+          </div>
+          <v-spacer></v-spacer>
+          <span class="close" @click="hideInputInfo">&times;</span></v-row
+        >
+        <div>
+          <label for="name">Tên khách sạn:</label>
+          <input type="text" id="name" v-model="info.name" />
+        </div>
+        <div>
+          <label for="phone">Hotline:</label>
+          <input type="text" id="phone" v-model="info.phone" />
+        </div>
+        <div>
+          <label for="address">Địa chỉ:</label>
+          <input type="text" id="address" v-model="info.address" />
+        </div>
+        <div>
+          <button class="save-button" @click="saveInfo">Lưu</button>
+        </div>
+      </form>
     </div>
   </nav>
 </template>
 
 <script>
+// import { is } from "core-js/core/object";
+
 export default {
   name: "AppNavbar",
   data() {
     return {
       dropdownVisible: false,
+      isInputInfo: false,
       isPriceModalVisible: false,
-      singleRoomPrice: "",
-      doubleRoomPrice: "",
-      tripleRoomPrice: "",
-      quadRoomPrice: "",
+      roomPrice: {
+        singleRoomPrice: "",
+        doubleRoomPrice: "",
+        tripleRoomPrice: "",
+        quadRoomPrice: "",
+      },
+      info: {
+        name: "",
+        phone: "",
+        address: "",
+      },
     };
   },
   methods: {
@@ -103,19 +149,35 @@ export default {
     hidePriceModal() {
       this.isPriceModalVisible = false;
     },
+    showInputInfo() {
+      this.isInputInfo = true;
+    },
+    hideInputInfo() {
+      this.isInputInfo = false;
+    },
     savePrices() {
       this.$toast.success("Lưu giá phòng thành công");
       this.hidePriceModal();
+    },
+    saveInfo() {
+      this.$toast.success("Lưu thông tin thành công");
+      this.hideInputInfo();
     },
   },
 };
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 .title {
   margin-bottom: 20px;
 }
 .save-button {
+  width: 100%;
   background-color: #007bff; /* Màu nền */
   color: white; /* Màu chữ */
   border: none; /* Bỏ viền */
@@ -299,5 +361,68 @@ i {
   border: 1px solid #ccc;
   padding: 5px;
   flex: 1;
+}
+.form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.logo img {
+  max-width: 100px;
+  margin-bottom: 10px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+form {
+  text-align: left;
+  background-color: #ffffff;
+  padding: 30px;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  width: 100%;
+  display: grid;
+  gap: 15px;
+  font-family: "Roboto", sans-serif;
+}
+
+label {
+  font-weight: 500;
+  font-size: 1rem;
+  color: #333;
+}
+
+input {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  font-size: 1rem;
+  background-color: #f9fafb;
+  transition: border-color 0.3s;
+}
+input:focus {
+  border-color: #4caf50;
+  outline: none;
+  box-shadow: 0 0 5px rgba(76, 175, 80, 0.2);
+}
+
+button:hover {
+  background-color: #45a049;
+  transform: translateY(-2px);
+}
+button:active {
+  transform: translateY(0);
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ const routes = [
     name: "Register",
     component: RegisterPage,
   },
+
   {
     path: "/dashboard",
     component: DashboardPage,
