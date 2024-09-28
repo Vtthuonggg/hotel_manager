d21712a06f600e2614db89cd674709f83cb38ead
Vtthuonggg <thuong23042003@gmail.com>
2024-09-28 12:13:24 +0700

    sua setting

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,15 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    allowedHosts: 'all', // Cho phép tất cả các host
+    client: {
+      webSocketURL: {
+        hostname: 'localhost',
+        pathname: '/ws',
+        port: 8080,
+        protocol: 'wss',
+      },
+    },
+  },
 });
